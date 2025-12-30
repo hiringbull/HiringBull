3ed9c5c9752786e77eb2072f494058cd0b606f9e
HiringBull - by @nayakpenguin <hiringbull.primary@gmail.com>
2025-12-30 14:57:54 +0530

    Update index.js
    added some changes for port issue in aws

--- server/src/index.js
+++ server/src/index.js
@@ -20,7 +20,11 @@ app.get("/", (req, res) => {
 });
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins for public access
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
@@ -40,6 +44,7 @@ app.use("/api", routes);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`HiringBull server running on port ${PORT}`);
+  console.log(`Server accessible at http://0.0.0.0:${PORT}`);
 });
